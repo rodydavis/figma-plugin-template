c232f32e49df7d80177aa640e6f9e1e08ed01df0
Rody Davis <31253215+rodydavis@users.noreply.github.com>
2022-03-07 17:53:52 -0800

    Update ui.ts

--- src/ui.ts
+++ src/ui.ts
@@ -1,13 +1,23 @@
-import "./style.css";
 import { html, css, LitElement } from "lit";
 import { customElement } from "lit/decorators.js";
+import { pluginWidth, pluginHeight } from "../src/constants";
 
 @customElement("my-plugin")
 export class MyPlugin extends LitElement {
-  static styles = css``;
+  static styles = css`
+    main {
+      border: 1px solid red;
+      --width: ${pluginWidth}px;
+      --height: ${pluginHeight - 1}px;
+      width: calc(var(--width) - 2px);
+      height: calc(var(--height) - 2px);
+    }
+  `;
 
   render() {
-    return html`My Plugin`;
+    return html`<main>
+      <h1>Hello, world!</h1>
+    </main>`;
   }
 }
 
